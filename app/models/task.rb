66c02e5e49e49835905ded0e8d0d5d7bbbb285a7
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2017-03-12 10:52:32 +0100

    Added link detection

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
+
+    FORMAT = AutoHtml::Pipeline.new(
+    	AutoHtml::HtmlEscape.new,
+    	AutoHtml::Image.new,
+    	AutoHtml::YouTube.new(width: "100%", height: "250"),
+    	AutoHtml::Link.new(target: "_blank", rel: "nofollow"),
+    	AutoHtml::SimpleFormat.new)
+
     belongs_to :user
+
+    def content=(t)
+    	super(t)
+    	self[:content_html] = FORMAT.call(t)
+    end
 end
