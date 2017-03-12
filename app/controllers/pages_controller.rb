855f3fb1435fe8ea0efe78f7f106176389c04606
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2017-03-12 08:33:00 +0100

    Applied design to tasks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,4 @@ class PagesController < ApplicationController
   def about
   end
 
-  def test
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   # get 'pages/home'
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'test' => 'pages#test'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
